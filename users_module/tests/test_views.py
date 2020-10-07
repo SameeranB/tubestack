89ab52dfe85e5112ceb7dcb8f4b0f1c4adfbb02f
SameeranB <sameeranbandishti@ieee.org>
2020-10-07 20:26:03 +0530

    Remove Email verification and corresponding tests

--- users_module/tests/test_views.py
+++ users_module/tests/test_views.py
@@ -1,6 +1,4 @@
-import pdb
 from allauth.account.models import EmailAddress
-
 from rest_framework import status
 
 from users_module.tests.test_setup import TestSetup
@@ -16,11 +14,6 @@ class TestViews(TestSetup):
         res = self.client.post(self.register_url)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_user_login_failure_unverified(self):
-        self.client.post(self.register_url, self.user_registration_data, format="json")
-        res = self.client.post(self.login_url, self.user_data, format='json')
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
     def test_user_login_success(self):
         res = self.client.post(self.register_url, self.user_registration_data, format="json")
         email = self.user_data['email']
@@ -29,5 +22,3 @@ class TestViews(TestSetup):
         user.save()
         res = self.client.post(self.login_url, self.user_data, format='json')
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-
